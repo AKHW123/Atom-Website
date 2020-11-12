96a833c6e1e2eab35838de550543a604312a1b35
AKHW123 <CeilingFan845@gmail.com>
2020-11-12 09:43:55 -0800

    made improvements during class
    added click function, separated css and js files, made some small style changes

---
+++ public/main.js
@@ -0,0 +1,5 @@
+function changeSomeHtml(){
+  $("#NHL").html("This is a page where I basically write about whatever I want. You can read what I write, or not, it's not that important stuff.");
+};
+
+$("#NHL").click(changeSomeHtml);
