28952aa45e753c7f400724d8ee7bcf4f41f77de5
AKHW123 <CeilingFan845@gmail.com>
2020-11-16 20:47:41 -0800

    important changes
    a big update from the last couple days

--- public/main.js
+++ public/main.js
@@ -1,5 +1,15 @@
 function changeSomeHtml(){
-  $("#NHL").html("This is a page where I basically write about whatever I want. You can read what I write, or not, it's not that important stuff.");
+  $("#NHL").html("This is a page where I basically post about whatever I want. You can check out what I write and provide links to, or not, it's not that important.");
 };
+function openGame(){
+  $("#game_button").click(requestGameContent)
+}
+function requestGameContent(){
+  $.ajax({url: "game.html", success: loadGameContent});
+}
+function loadGameContent(result){
+  $("#game_content").html(result);
+}
 
 $("#NHL").click(changeSomeHtml);
+$("#game_button").click(requestGameContent);
